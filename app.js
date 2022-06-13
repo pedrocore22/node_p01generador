6cba3a648469eca75ab6cc6b74245dd501d17cbd
pedrocore22 <pedrocore22@itweblearning.com>
2022-06-13 13:17:20 +0200

    queries

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Script para generar instrucciones de
 // inserción de proveedores en MySQL
 
+const { v4: uuidv4} = require('uuid');
+const fs = require('fs');
+
 const names = [
     'Transportes', 
     'Distribuciones', 
@@ -114,15 +117,67 @@ const surnames = [
     'Rojas'
 ];
 
-let vendors = [];
+let streets = [
+    'Gran Vía',
+    'Av. de España',
+    'Av. de la Constitución',
+    'Av. de América',
+    'Clara Campoamor',
+    'Ramón y Cajal',
+    'ACDC',
+    'Las Margaritas',
+    'Av. de París',
+    'Av. de la Hispanidad'
+]
 
-for (i = 0; i < 200; i++) {
-    const name = names[Math.floor(Math.random() * names.length)];
-    const surname = surnames[Math.floor(Math.random() * surnames.length)];
-    vendors.push({
-        nombre: name + ' ' + surname + ', S.L.',
-        cif: 'A12345566'
-    })
+let cities = [
+    'Madrid',
+    'Barcelona',
+    'Valencia',
+    'Cáceres',
+    'Albacete',
+    'Teruel',
+    'Bilbao',
+    'Badajoz',
+    'Soria',
+    'Salamanca'
+]
+
+let queries = [];
+
+const deleteDiacritics = text => {
+    const textWithoutDiacritics = text.replace(/á/ig, 'a')
+                                      .replace(/é/ig, 'e')
+                                      .replace(/í/ig, 'i')
+                                      .replace(/ó/ig, 'o')
+                                      .replace(/ú/ig, 'u')
+                                      .replace(/ü/ig, 'u');
+    return textWithoutDiacritics;
 }
 
-console.log(vendors);
+for (i = 0; i < 200; i++) {
+    const id = uuidv4().toString();
+    const name = names[Math.floor(Math.random() * names.length)];
+    const surname = surnames[Math.floor(Math.random() * surnames.length)];
+    const cif = 'B' + (Math.random()).toString().slice(2,10);
+    const address = streets[Math.floor(Math.random() * streets.length)] +
+                    ', ' + Math.ceil(Math.random() * 10);
+    const city = cities[Math.floor(Math.random() * cities.length)];
+    const email = 'info@' + name.toLowerCase() + surname.toLowerCase() +
+                  '.com';
+    const phone = '+346' + (Math.random()).toString().slice(2,10);
+    const query = "INSERT INTO proveedores (id, nombre, cif, direccion, localidad, email, telefono, fecha_alta)" +
+                  " VALUES('" + id + "','" +
+                  name + ' ' + surname + ', S.L.' + "','" +
+                  cif + "','" +
+                  address + "','" +
+                  city + "','" +
+                  deleteDiacritics(email) + "','" +
+                  phone + "'" +
+                  ",CURDATE());";
+    queries.push(query);
+}
+
+const queriesText = queries.join('\n');
+
+fs.writeFileSync('queries.txt',  queriesText, 'utf-8');
